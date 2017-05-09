881e16c49d411d8eef0b91b65ba6cdae3478e2a0
joao_felipe <joaofelipe1294@gmail.com>
2017-05-09 00:49:16 -0300

    Concluida classe que implementa algoritmo template matching

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from base.base import load
 import cv2
 import numpy as np
+from pattern_recognition.template_matching import TemplateMatching
 
-images, labels, names = load('bases/digits/train', 'bases/digits/train.txt')
+
+train_images, train_labels, train_names = load('bases/digits/train', 'bases/digits/train.txt')
+test_images, test_labels, test_names = load('bases/digits/val', 'bases/digits/val.txt')
 #print(labels)
 
+TemplateMatching(train_images, train_labels, test_images, test_labels).apply()
 
+
+'''
 base_labels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 for label in labels:
@@ -23,3 +29,4 @@ avrage_image = np.array(avrage_image, np.uint8)
 print(base_labels)
 cv2.imshow('mean_image', avrage_image)
 cv2.waitKey(0)
+'''
---
+++ pattern_recognition/template_matching.py
@@ -0,0 +1,44 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+class TemplateMatching(object):
+
+	def __init__(self, train_values, train_labels, test_values, test_labels):
+		self.train_values = train_values
+		self.train_labels = train_labels
+		self.test_values = test_values
+		self.test_labels = test_labels
+
+
+	def prepare_values(self, values):
+		prepared_values = []
+		for image in values:
+		 	prepared_values.append(np.ravel(image))
+	 	return prepared_values
+
+	def apply(self):
+	 	train = self.prepare_values(self.train_values)
+	 	test = self.prepare_values(self.test_values)
+	 	neigh = KNeighborsClassifier(n_neighbors=3)
+	 	print('Treinando KNN ...')
+	 	neigh.fit(train, self.train_labels)
+	 	print('KNN treinado')
+	 	print('Classificando imagens ...')
+	 	labels = neigh.predict(test)
+	 	print('Classificacao concluida')
+	 	self.calc_precision(labels)
+	 	self.calc_confusion_matrix(labels)
+
+ 	def calc_precision(self, labels):
+		corrects = 0
+		for index in xrange(0 , len(labels)):
+		 	if labels[index] == self.test_labels[index]:
+		 		corrects += 1
+ 		print('Precision : ' + str(float(corrects) / float(len(self.test_labels))))
+
+	def calc_confusion_matrix(self, labels):
+		confusion_matrix = np.zeros((10,10), np.uint32)
+		for index in xrange(0, len(self.test_labels)):
+			confusion_matrix[self.test_labels[index], labels[index]] += 1
+		print('Confusion Matrix : ')
+		print(confusion_matrix)
