5f86231a4e2ff73962e9540b8f943ee23a5ad061
joao_felipe <joaofelipe1294@gmail.com>
2017-05-09 02:23:34 -0300

    Adicionada implementacao de template matching utilizando comparacao com a imagem media de cada classe

--- main.py
+++ main.py
@@ -10,26 +10,5 @@ test_path = sys.argv[2]
 train_images, train_labels, train_names = load(train_path, train_path + '.txt')
 test_images, test_labels, test_names = load(test_path,  test_path + '.txt')
 
+TemplateMatching(train_images, train_labels, test_images, test_labels).apply_with_avrage_images()
 TemplateMatching(train_images, train_labels, test_images, test_labels).apply()
-
-
-
-'''
-base_labels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-for label in labels:
-	base_labels[label] += 1
-
-avrage_image = np.zeros(images[0].shape, np.uint64)
-count = 0
-for index in xrange(0, len(images)):
-	if labels[index] == 8:
-		avrage_image += images[index]
-		count += 1
-
-avrage_image /= count
-avrage_image = np.array(avrage_image, np.uint8)
-print(base_labels)
-cv2.imshow('mean_image', avrage_image)
-cv2.waitKey(0)
-'''
--- pattern_recognition/template_matching.py
+++ pattern_recognition/template_matching.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+import cv2
 
 class TemplateMatching(object):
 
@@ -10,7 +11,8 @@ class TemplateMatching(object):
 		self.test_values = test_values
 		self.test_labels = test_labels
 		self.K = 0
-
+		self.avrage_images = []
+		self.avrage_labels = []
 
 	def read_values(self):
 		if sys.version_info[0] == 2:
@@ -51,3 +53,30 @@ class TemplateMatching(object):
 			confusion_matrix[self.test_labels[index], labels[index]] += 1
 		print('Confusion Matrix : ')
 		print(confusion_matrix)
+
+	def calc_mean_images(self):
+		for label in range(0,10):
+			avrage_image = np.zeros(self.train_values[0].shape, np.uint64)
+			count = 0
+			for index in range(0, len(self.train_values)):
+				if self.train_labels[index] == label:
+					avrage_image += self.train_values[index]
+					count += 1
+			avrage_image /= count
+			avrage_image = np.array(avrage_image, np.uint8)
+			self.avrage_images.append(avrage_image)
+			self.avrage_labels.append(label)
+
+	def apply_with_avrage_images(self):
+		self.calc_mean_images()
+		train = self.prepare_values(self.avrage_images)
+		test = self.prepare_values(self.test_values)
+		neigh = KNeighborsClassifier(n_neighbors = 1)
+		print('Treinando KNN ...')
+		neigh.fit(train, self.avrage_labels)
+		print('KNN treinado')
+		print('Classificando imagens ...')
+		labels = neigh.predict(test)
+		print('Classificacao concluida')
+		self.calc_precision(labels)
+		self.calc_confusion_matrix(labels)
