a969e313cd53e6f785ae70cd843f9a222c51e183
joao_felipe <joaofelipe1294@gmail.com>
2017-05-09 18:38:36 -0300

    Adicionada verificacao do tempo gasto na execucao do sistema

--- main.py
+++ main.py
@@ -10,5 +10,7 @@ test_path = sys.argv[2]
 train_images, train_labels, train_names = load(train_path, train_path + '.txt')
 test_images, test_labels, test_names = load(test_path,  test_path + '.txt')
 
+start_time = time.time()
 TemplateMatching(train_images, train_labels, test_images, test_labels).apply_with_avrage_images()
 #TemplateMatching(train_images, train_labels, test_images, test_labels).apply()
+print("arquivo treino --- %s seconds ---" % (time.time() - start_time))
